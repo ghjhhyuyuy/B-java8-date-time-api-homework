a3dc9b0d776d69318ba3f2083f5d06faf3bcb46a
zuowen.Wang <798389920@qq.com>
2020-09-17 22:25:07 +0800

    Finish Practice1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -11,6 +12,10 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    LocalDate localDate = LocalDate.of(date.getYear(),5,1);
+    if(localDate.isBefore(date)){
+      return localDate.plusYears(1).toEpochDay() - date.toEpochDay();
+    }
+    return localDate.toEpochDay() - date.toEpochDay();
   }
 }
