59ca0d42e64e5da9eeab8cac4899bfcac8e0d608
zuowen.Wang <798389920@qq.com>
2020-09-17 22:40:12 +0800

    Finish Practice2

--- src/main/java/com/thoughtworks/capability/gtb/Practice2.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice2.java
@@ -1,6 +1,10 @@
 package com.thoughtworks.capability.gtb;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 对任意日期获取下一个工作日, 不考虑节假日
@@ -11,6 +15,15 @@ import java.time.LocalDate;
 public class Practice2 {
 
   public static LocalDate getNextWorkDate(LocalDate date) {
-    return null;
+    DayOfWeek dayOfWeek = date.getDayOfWeek();
+    String[] workDayIsNextDay = {"MONDAY","TUESDAY","WEDNESDAY","THURSDAY","SUNDAY"};
+    List<String> dayString = Arrays.stream(workDayIsNextDay).filter(day -> day.equals(dayOfWeek.name())).collect(Collectors.toList());
+    if(dayString.size() != 0){
+      return date.plusDays(1);
+    }
+    if(dayOfWeek.name().equals("FRIDAY")){
+      return date.plusDays(3);
+    }
+    return date.plusWeeks(2);
   }
 }
